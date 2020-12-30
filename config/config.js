ed70462f5501f3ca663fe18227fa408847d88a4a
8283792 <8283792@qq.com>
2020-12-30 14:24:49 +0800

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,12 @@
+const config = {
+  // apiBaseUrl: 'http://api.sleeve.7yue.pro/v1',
+  apiBaseUrl: 'http://se.talelin.com/v1/',
+  appkey: 'UZwOyxQkcPbWQPPR',
+  // clientkey: 'C4C3C6BCF13C96C1F5CD2B2634512FDC'
+  // apiBaseUrl: 'http://se.7yue.pro/v1',
+  // appkey: '3JuxueyyErw6nhiD'
+}
+
+export {
+  config
+}
